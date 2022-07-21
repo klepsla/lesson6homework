0e75355dfa33efec2dd6df4bc8c146207e948e00
klepsla <kleslava3@gmail.com>
2022-07-21 18:12:24 +0300

    Задание № 1 выполнено.

--- src/Main.java
+++ src/Main.java
@@ -3,17 +3,20 @@ public class Main {
         System.out.println("Hello world!");
 
 
+        //Задание № 1
+
+        int [] weight = new int []{-1, 2, 3};
+        System.out.println(weight[0]);
+        System.out.println(weight[1]);
+        System.out.println(weight[2]);
+
+        double [] arr = {-1.57, 7.654, 9.986};
+        System.out.println(arr[0]);
+
+        int [] year = {21, 45, 12, 98, 121, 37, 99, 1001};
 
 
-
-
-
-
-
-
-
-
-
+        //Задание № 2
 
 
 
